8b765b1cd7099e3dfde5148d209e3c1d0805b7ad
recypher-zz <kniemerg001@me.com>
2025-03-14 15:06:33 -0400

    Updating of sample name spacing

--- SamplePlugin/AutoSynthesis.cs
+++ SamplePlugin/AutoSynthesis.cs
@@ -6,7 +6,7 @@ using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
 using SamplePlugin.Windows;
 
-namespace SamplePlugin;
+namespace AutoSynthesis;
 
 public sealed class Plugin : IDalamudPlugin
 {
